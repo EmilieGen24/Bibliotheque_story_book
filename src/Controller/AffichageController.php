5ca5fd63930f0a7bafb3595e5bfd56fa735aa996
Emilie Geneste <mimigeneste@gmail.com>
2026-01-14 10:58:47 +0100

    first commit

---
+++ src/Controller/AffichageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AffichageController extends AbstractController
+{
+    #[Route('/affichage', name: 'affichage')]
+    public function index(): Response
+    {
+        return $this->render('affichage/affichage.html.twig', [
+            'controller_name' => 'AffichageController',
+        ]);
+    }
+}
